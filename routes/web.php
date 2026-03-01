5da1013dd0a7783aa55a9c912a481d54ecd9f2cf
bebeorca <dimasrdyo@gmail.com>
2026-03-01 16:44:20 +0700

    setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', fn() => response()->json(
+    [
+        "status" => "200",
+        "active_endpoints" => [
+            "Documentation" => [
+                "GET" => "/docs"
+            ],
+            "Authentication" => [
+                "POST" => [
+                    "/signup" => [
+                        "name" => "Nama",
+                        "email" => "user@mail.com",
+                        "phone" => "08xxxxx",
+                        "password" => "password123"
+                    ],
+                    "/signin" => [
+                        "email" => "user@mail.com",
+                        "password" => "password123"
+                    ],
+                ],
+                "GET" => "/profile",
+                "PUT" => [
+                    "/editme",
+                    "/changepassword"
+                ]
+            ],
+            "History" => [
+                "POST" => "/savehistory",
+                "GET" => "/history"
+            ]
+        ]
+    ]
+));
